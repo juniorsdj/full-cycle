142e51468e8ef31cd900bfd78e4aa6660cdd6c35
Junior Santos <jddsjr98@gmail.com>
2021-07-07 08:26:59 -0300

    ci(challenge-ci): :sparkles: add subtraction and multiply fn

--- challenge-ci/src/sum.js
+++ challenge-ci/src/sum.js
@@ -1,5 +1,15 @@
 const sum = (a, b) => {
   return a + b;
 };
+const subtraction = (a, b) => {
+  return a - b;
+};
+const multiply = (a, b) => {
+  return a * b;
+};
 
-module.exports = sum
+module.exports = {
+  sum,
+  subtraction,
+  multiply,
+};
--- challenge-ci/src/sum.spec.js
+++ challenge-ci/src/sum.spec.js
@@ -1,5 +1,11 @@
-const sum = require("./sum");
+const { sum, subtraction, multiply } = require("./sum");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+test("adds 2 - 1 to equal 1", () => {
+  expect(subtraction(2, 1)).toBe(1);
+});
+test("adds 1 * 2 to equal 2", () => {
+  expect(multiply(1, 2)).toBe(2);
 });
